1be24003768feb69b50a6c190a166b0084dfdafc
IzaiAnda <44356898+IzaiAnda@users.noreply.github.com>
2021-03-13 13:14:23 +0100

    CreatingLoading

--- Assets/Script/CreateManager.cs
+++ Assets/Script/CreateManager.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 public class CreateManager : MonoBehaviour
 {
     public Transform tile;
-    public GameObject floor;
+    GameObject floor;
+
+    public List<GameObject> goList;
+
     void Start()
     {
         floor = new GameObject();
         floor.name = "floor";
+
         for (int i = 0; i <= 10; i++)
         {
             for (int j = 0; j <= 10; j++)
@@ -19,8 +23,31 @@ public class CreateManager : MonoBehaviour
 
                 go.transform.position = new Vector3(i, 0, j);
                 go.transform.parent = floor.transform;
+                go.name = i + "-" + j;
             }
         }
+    }
+
+    public void StreetLoader(GameObject street)
+    {
+        goList = new List<GameObject>();
+        for (int i = 0; i <= 10; i++)
+        {
+            for (int j = 0; j <= 10; j++)
+            {
+                GameObject go = GameObject.Find("/street/"+ i + "-" + j);
+                if (go != null)
+                {
+                    goList.Add(go);
+                }
+            }
+        }
+        goList.Sort(SortByName);
+    }
+
+    static int SortByName(GameObject p1, GameObject p2)
+    {
+        return p1.name.CompareTo(p2.name);
     }
 
     // Update is called once per frame
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+    public float rayLength;
+    public LayerMask layerMask;
+    public GameObject baseTurr;
+    public GameObject castle;
+    public GameObject wP;
+    GameObject selected;
+    public GameObject spawn;
+    GameObject street;
+    bool streetSelected = false;
+
+    private void Awake()
+    {
+        selected = castle;
+        street = new GameObject();
+        street.name = "street";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            selected = baseTurr;
+            Debug.Log(selected.name);
+            streetSelected = false;
+        }
+        else if(Input.GetKeyDown(KeyCode.I))
+        {
+            selected = castle;
+            streetSelected = false;
+            Debug.Log(selected.name);
+        }
+        else if(Input.GetKeyDown(KeyCode.O))
+        {
+            selected = wP;
+            streetSelected = true;
+            Debug.Log(selected.name);
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+            selected = spawn;
+            streetSelected = false;
+            Debug.Log(selected.name);
+        }
+
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit, rayLength,layerMask))
+            {
+                switch (hit.collider.gameObject.tag)
+                {
+                    case "Tile":
+                        Debug.Log(hit.collider.transform.position);
+                        GameObject go = Instantiate(selected, hit.collider.transform.position, hit.collider.transform.rotation);
+                        //go.transform.parent = hit.collider.transform.parent;
+                        if (streetSelected)
+                        {
+                            go.transform.parent = street.transform;
+                            go.name = hit.collider.gameObject.name;
+                        }
+                        Destroy(hit.collider.transform.gameObject);
+                        break;
+                    case "CreateManager":
+                        hit.collider.gameObject.GetComponent<CreateManager>().StreetLoader(street);
+                        break;
+                }
+            }
+        }
+    }
+}
